96e426ecdd19b4aa593dbd008453af21ec922e48
Tống Phan Kim Thạch <22637901.thach@student.iuh.edu.vn>
2026-01-02 13:15:56 +0700

    style: Format metro.config.js

---
+++ metro.config.js
@@ -0,0 +1,15 @@
+const { getDefaultConfig } = require('expo/metro-config');
+const path = require('path');
+
+const config = getDefaultConfig(__dirname);
+
+// Thêm resolver cho path alias
+config.resolver = {
+  ...config.resolver,
+  alias: {
+    '@': path.resolve(__dirname),
+  },
+};
+
+module.exports = config;
+
